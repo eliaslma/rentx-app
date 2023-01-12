9df4db9138e75298dfa4f3cf2ca65df58ec3bcbb
Elias Lima <sci.eliaslima@gmail.com>
2023-01-12 00:09:15 -0300

    feat: Set application global theme

---
+++ src/global/styles/styles.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+import theme from './theme';
+
+declare module 'styled-components' {
+    type ThemeType = typeof theme // copiou os tipos de theme para dentro do Themetype com typeof
+
+    export interface DefaultTheme extends ThemeType {} // default theme extende nosso tema
+}
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,31 @@
+export default {
+    colors: {
+        header: "#1B1B1F",
+
+        background_primary: "#F4F5F6",
+        background_secondary: "#FFFFFF",
+
+        text: "#7A7A80",
+        text_detail: "#AEAEB3",
+        title: "#47474D",
+
+        line: "#EBEBF0",
+
+        main: "#DC1637",
+        main_light: "#FDEDEF",
+        success: "#03B252",
+
+        shape: "#E1E1E8",
+        shape_dark: "#29292E",
+        
+    },
+    fonts: {
+        inter_regular: "Inter_400Regular",
+        inter_medium: "Inter_500Medium",
+        
+        archivo_regular: "Archivo_400Regular",
+        archivo_medium: "Archivo_500Medium",
+        archivo_semibold: "Archivo_600SemiBold"
+    }
+
+}
