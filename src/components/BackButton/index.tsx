eb0e360bc98f7ec9d2138cb39afcf02d65e0fee9
Elias Lima <sci.eliaslima@gmail.com>
2023-01-14 23:47:16 -0300

    feat: Create navigation back button

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { BorderlessButtonProps } from 'react-native-gesture-handler';
+
+import {
+    Container,
+    Icon
+} from './styles';
+
+interface Props extends BorderlessButtonProps{
+    color?: string;
+}
+
+export function BackButton({color, ...rest}: Props){
+
+    const theme = useTheme()
+
+    return(
+        <Container>
+            <Icon name="keyboard-arrow-left" color={color ? color : theme.colors.text}/>
+        </Container>
+    );
+}
