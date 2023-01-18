5f1811fe9bca975a588ffc0f9ac454aa4bddefb1
Elias Lima <sci.eliaslima@gmail.com>
2023-01-17 22:53:57 -0300

    style: Change flat list styling attributes

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,10 @@
 import { RFValue } from 'react-native-responsive-fontsize';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
+export const Container = styled(GestureHandlerRootView)`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background_primary};
 `;
@@ -17,7 +20,7 @@ export const HeaderContent = styled.View`
     align-items: center;
     justify-content: space-between;
     width: 100%;
-    padding: 40px 24px;
+    padding: 30px 24px;
 `;
 
 export const TotalCars = styled.Text`
@@ -30,4 +33,15 @@ export const LoadContainer = styled.View`
     flex: 1;
     align-items: center;
     justify-content: center;
+`;
+
+export const CarList = styled(FlatList).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle: {
+        paddingBottom: getBottomSpace()
+    }
+})`
+    flex: 1;
+    padding: 16px 16px 0px;
+
 `;
