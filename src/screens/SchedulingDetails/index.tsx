092adb97c72ba198fddef0a3909be5bba9ce71fa
Elias Lima <sci.eliaslima@gmail.com>
2023-01-22 22:19:55 -0300

    feat: Display vehicle data and specification icons dynamically

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { isIphoneX, getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 import { Feather } from '@expo/vector-icons'
 import { useTheme } from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+import { useRoute } from '@react-navigation/native';
+import { getSpecIcon } from '@myapp/utils/getSpecIcon';
 
 import speed from '../../assets/speed.svg'
 import acceleration from '../../assets/acceleration.svg'
@@ -10,7 +13,7 @@ import gasoline from '../../assets/gasoline.svg'
 import exchange from '../../assets/exchange.svg'
 import people from '../../assets/people.svg'
 import Calendar from '../../assets/calendar.svg'
-import Arrow from '../../assets/arrow.svg'
+
 
 import { DefaultButton } from '@myapp/components/DefaultButton';
 import { CarSpec } from '@myapp/components/CarSpec';
@@ -46,38 +49,50 @@ import {
     Footer,
 
 } from './styles';
+import { CarDTO } from '@myapp/dtos/CarDTO';
+import { CardCar } from '@myapp/components/CardCar';
+
+interface Params {
+    car: CarDTO;
+}
 
 
-export function SchedulingDetails(){
+export function SchedulingDetails({navigation}){
 
     const theme = useTheme();
+    const route = useRoute()
+    const { car } = route.params as Params
+    
+    function handleConfirmScheduling(){
+        navigation.navigate('SchedulingSuccess')
+    }
 
     return(
         <Container>
+            <StatusBar style='dark' translucent={false} backgroundColor={'white'}/>
             <Header style={ isIphoneX() && {paddingTop: getStatusBarHeight()}}>
                 <HeaderContent>
-                    <BackButton onPress={() => {}}/>
+                    <BackButton onPress={() => navigation.goBack()}/>
                 </HeaderContent>
             </Header>
-            <ImageSlider imagesUrl={['https://belowinvoice.com/wp-content/uploads/2021/04/Lamborguine-Huracan-Coupe-Rosso-Mars.png']}/>
+            <ImageSlider imagesUrl={car.photos}/>
             <Content>
                 <Description>
                     <Model>
-                        <Brand>lamborguini</Brand>
-                        <Name>Huracan</Name>
+                        <Brand>{car.brand}</Brand>
+                        <Name>{car.name}</Name>
                     </Model>
                     <Rent>
-                        <Period>ao dia</Period>
-                        <Price>R$ 580</Price>
+                        <Period>{car.rent.period}</Period>
+                        <Price>R$ {car.rent.price}</Price>
                     </Rent>
                 </Description>
                 <ModelSpec>
-                    <CarSpec icon={speed} name={'380km/h'}/>
-                    <CarSpec icon={acceleration} name={'3.2s'}/>
-                    <CarSpec icon={horsePower} name={'800 HP'}/>
-                    <CarSpec icon={gasoline} name={'Gasolina'}/>
-                    <CarSpec icon={exchange} name={'Auto'}/>
-                    <CarSpec icon={people} name={'800 HP'}/>
+                    {
+                        car.accessories.map(spec => (
+                        <CarSpec icon={getSpecIcon(spec.type)} name={spec.name} key={spec.name} />
+                        ))
+                    }
                 </ModelSpec>
                 <Details>
                     <Date>
@@ -109,10 +124,9 @@ export function SchedulingDetails(){
                         <TotalPrice>R$ 2.900</TotalPrice>
                     </Daily>
                 </Details>
-                
             </Content>
             <Footer style={isIphoneX() && {paddingBottom: getBottomSpace()}}>
-                <DefaultButton title="Alugar agora" color={theme.colors.success}/>
+                <DefaultButton title="Alugar agora" color={theme.colors.success} onPress={handleConfirmScheduling}/>
             </Footer>        
         </Container>
     );
