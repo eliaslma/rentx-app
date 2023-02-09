983091b2219ca30cae43fa73214f6072ceeb8e88
Elias Lima <sci.eliaslima@gmail.com>
2023-02-09 18:48:16 -0300

    feat: Persisting and reading user authentication data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -5,6 +5,8 @@ import {
     ReactNode
 } from "react";
 
+import { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { api } from "@myapp/services/api";
 
 interface userData{
@@ -29,6 +31,7 @@ interface SignCredentials{
 interface AuthContextProps{
     user: userData
     signIn: (credentials: SignCredentials) => Promise<void>
+    userStorageLoading: boolean;
 }
 
 interface AuthProviderProps{
@@ -40,7 +43,9 @@ const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
 
 function AuthProvider({children} : AuthProviderProps){
 
+    const [userStorageLoading,setUserStorageLoading] = useState(true);
     const [userAuthState, setUserAuthState] = useState<authState>({} as authState)
+    const userStorageKey = '@gofinances:user';
 
     async function signIn(userCredential: SignCredentials){
         await api.post('/sessions', {
@@ -48,17 +53,46 @@ function AuthProvider({children} : AuthProviderProps){
             password: userCredential.password
         }).then((response) => {
             const { user, token } = response.data;
+            const userAuthenticade : authState = {
+                token: token,
+                user: user
+            }
             api.defaults.headers.authorization = `Bearer ${token}`;
             setUserAuthState({ user, token })
+            setAuthenticatedUserData(userAuthenticade)
         }).catch((error) => {
             throw Error(error)
         })
     }
 
+    async function setAuthenticatedUserData(userAuthenticad: authState){
+        try {
+            await AsyncStorage.setItem(userStorageKey,JSON.stringify(userAuthenticad))
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    async function getAuthenticatedUserData(){
+        
+            const userData = await AsyncStorage.getItem(userStorageKey) 
+            if(userData){
+                const userDataFormatted = JSON.parse(userData) as authState
+                api.defaults.headers.authorization = `Bearer ${userDataFormatted.token}`;
+                setUserAuthState(userDataFormatted)
+            }
+            setUserStorageLoading(false)
+    } 
+
+    useEffect(() => {
+        getAuthenticatedUserData(); 
+    },[])
+
     return(
         <AuthContext.Provider value={{
             user: userAuthState.user,
-            signIn
+            signIn,
+            userStorageLoading
         }}>
             {children}
         </AuthContext.Provider>
@@ -67,11 +101,9 @@ function AuthProvider({children} : AuthProviderProps){
     );
 }
 
-
 function useAuth(){
     const context = useContext(AuthContext)
     return context;
 }
-
 
 export {AuthProvider, useAuth}
